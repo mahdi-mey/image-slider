f46a32e934e976ae2f30cf084b311c6b60942e38
Mahdi <mahdimey17@gmail.com>
2023-11-19 15:26:05 +0330

    selected variables in js

---
+++ script.js
@@ -0,0 +1,6 @@
+const imageWrapper = document.querySelector('.image-wrapper')
+const sliderImages = document.querySelectorAll('.slider-container img')
+
+// buttons 
+const prevBtn = document.querySelector('.prev-wrapper')
+const nextBtn = document.querySelector('.next-wrapper')
